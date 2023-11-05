ae2b07f85874ae60f86a71322ec77c0f3a12fc1f
Kingseungil <flykim511@naver.com>
2023-11-05 23:06:01 +0900

    env 설정

---
+++ module-core/src/main/java/com/loan/core/config/PropertyConfig.java
@@ -0,0 +1,15 @@
+package com.loan.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources(
+  @PropertySource(
+    "classpath:properties/env.properties"
+  )
+)
+public class PropertyConfig {
+
+}
